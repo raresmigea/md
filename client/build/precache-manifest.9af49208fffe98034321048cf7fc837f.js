8c6d2422f8dacbe4f81e8278900d4184629bf98f
raresmigea <raresini@yahoo.com>
2020-09-20 18:49:27 +0300

    client server communication

---
+++ client/build/precache-manifest.9af49208fffe98034321048cf7fc837f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4956048c5c71442a9b3c",
+    "url": "/static/css/main.23fd8177.chunk.css"
+  },
+  {
+    "revision": "4956048c5c71442a9b3c",
+    "url": "/static/js/main.4956048c.chunk.js"
+  },
+  {
+    "revision": "caf926f8a02dbb89f48d",
+    "url": "/static/js/1.caf926f8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "099607ff52df78c0f5bc8a250fa11aa4",
+    "url": "/index.html"
+  }
+];
