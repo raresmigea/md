e5cf970a5008c1789033773c89dabab90dc61c54
raresmigea <raresini@yahoo.com>
2020-09-21 22:42:52 +0300

    convert the file to html and log it

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const showdown = require('showdown');
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+converter = new showdown.Converter();
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Expressas' });
@@ -14,5 +17,21 @@ app.post('/api/world', (req, res) => {
     `I received your POST request. This is what you sent me: ${req.body.post}`
   );
 });
+
+fs = require('fs');
+fs.readFile(__dirname + '/posts/react-v16.13.0.md', 'utf8', function (
+  err,
+  data
+) {
+  if (err) {
+    return console.log(err);
+  }
+  text = data;
+  html = converter.makeHtml(text);
+  // res.json(['markdown', html]);
+  // console.log(data);
+  console.log(html);
+});
+console.log('dd: ', __dirname);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
