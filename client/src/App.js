0c0beb28ecb153a83314c3d16e474c4060a1b730
raresmigea <raresini@yahoo.com>
2020-09-22 00:15:06 +0300

    get data

--- client/src/App.js
+++ client/src/App.js
@@ -8,18 +8,10 @@ class App extends Component {
     responseToPost: '',
   };
   componentDidMount() {
-    this.callApi()
-      .then((res) => this.setState({ response: res.express }))
-      .catch((err) => console.log(err));
+    this.getData();
   }
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    return body;
-  };
-  handleSubmit = async (e) => {
-    e.preventDefault();
+
+  getData = async (e) => {
     const response = await fetch('/api/world', {
       method: 'POST',
       headers: {
@@ -33,18 +25,6 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type='text'
-            value={this.state.post}
-            onChange={(e) => this.setState({ post: e.target.value })}
-          />
-          <button type='submit'>Submit</button>
-        </form>
         <p>{this.state.responseToPost}</p>
       </div>
     );
