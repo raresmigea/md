5e144c3a271dac648f76dc56ebf9fd5302f67f94
raresmigea <raresini@yahoo.com>
2020-09-25 19:27:38 +0300

    removed extra line

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-
 import Page from './components/Page';
 import './App.css';
 class App extends Component {
