e9cff4784c45a192e44c05f0ca13ef6aeb99ab0a
raresmigea <raresini@yahoo.com>
2020-10-01 23:55:55 +0300

    extra line after declarations

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Page from './components/Page';
 import './App.css';
+
 class App extends Component {
   state = {
     response: '',
